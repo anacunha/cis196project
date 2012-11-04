f8051b7e6bb7a30ab10eeb0ffd7099ceda8dbc36
Ana Cunha <analza.cunha@gmail.com>
2012-11-04 01:18:28 -0500

    Fixed paths on integration tests

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -6,32 +6,23 @@ describe "Users" do
     fixtures :users
     
     before do
-      sign_in
-      visit users_path
-      click_link "New User"
+      visit root_path
+      click_on "Sign Up"
     end
     
     it "name can't be blank", js:true do
       fill_in "Email", with: "ana@cunha.com"
-      click_button "Create User"      
+      click_button "Sign up"      
       error_message = "Name can't be blank"
       page.should have_content(error_message)
     end
     
     it "email can't be blank", js:true do
       fill_in "Name", with: "Ana"
-      click_button "Create User"      
+      click_button "Sign up"      
       error_message = "Email can't be blank"
       page.should have_content(error_message)
     end
-    
-    it "email has to be unique", js:true do
-      fill_in "Name", with: "Ana"
-      fill_in "Email", with: "ana@email.com"
-      click_on "Create User"
-      error_message = "Email has already been taken"
-      page.should have_content(error_message)
-    end
-    
+
   end
 end
